5e2b7f20291d8dbc4c84654fd7d1fd25d55e4819
Suraj0419 <suraj.pai@dhruvts.com>
2024-06-27 11:13:09 +0530

    added changes

--- update-config.ps1
+++ update-config.ps1
@@ -1,23 +1,22 @@
 param (
-    [string]$appSettingsPath,
-    [string]$dbServer,
-    [string]$dbName,
-    [string]$dbUser,
-    [string]$dbPassword
+  [string]$appSettingsPath,
+  [string]$dbServer,
+  [string]$dbName,
+  [string]$dbUser,
+  [string]$dbPassword
 )
 
 # Read the content of appsettings.json
 $jsonContent = Get-Content -Path $appSettingsPath -Raw | ConvertFrom-Json
 
-if (-not $jsonContent.PSObject.Properties['ConnectionStrings']) {
-    $jsonContent | Add-Member -MemberType NoteProperty -Name 'ConnectionStrings' -Value @{}
+# Check if the 'ConnectionStrings' section exists and create it if not
+if (-not $jsonContent.ContainsKey('ConnectionStrings')) {
+  $jsonContent | Add-Member -MemberType NoteProperty -Name 'ConnectionStrings' -Value @{}
 }
 
-# Ensure DefaultConnection property exists
-if (-not $jsonContent.ConnectionStrings.PSObject.Properties['DefaultConnection']) {
-    
-    $jsonContent.ConnectionStrings | Add-Member -MemberType NoteProperty -Name 'DefaultConnection' -Value @{}
-}
+$jsonContent.ConnectionStrings | Add-Member -MemberType NoteProperty -Name 'DefaultConnection' -Value @{}
+
+# Set the connection string for 'DefaultConnection'
 $jsonContent.ConnectionStrings.DefaultConnection = "Server=$dbServer;Database=$dbName;User Id=$dbUser;Password=$dbPassword;"
 
 # Convert the JSON object back to a string
